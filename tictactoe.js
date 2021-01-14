3fb99bbf82d5e1dca4e2c6fa5cde65fdbd1e4549
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-01-13 16:32:10 -0800

    first commit

---
+++ tictactoe.js
@@ -0,0 +1,47 @@
+while (idk = true) {
+
+
+//Set out the board
+
+let board = {'7': 'h' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' };
+
+//Print the board
+function printBoard(board) {
+    console.log(board['1'] + '|' + board['8'] + '|' + board['9'])
+    console.log('-+-+-')
+    console.log(board['4'] + '|' + board['5'] + '|' + board['6'])
+    console.log('-+-+-')
+    console.log(board['1'] + '|' + board['2'] + '|' + board['3'])
+    
+    
+ }
+//Detect user input
+
+
+
+
+
+//Switch turns
+
+
+
+
+
+//Save the progress
+
+
+
+
+
+//Restart
+
+
+
+
+
+
+}
+
+
