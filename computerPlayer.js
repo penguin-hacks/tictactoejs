b223c2bda36f494e27c05d3964cb64368e71b174
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-01-19 16:41:47 -0800

    debugging

--- computerPlayer.js
+++ computerPlayer.js
@@ -1,17 +1,24 @@
 let board = {'7': 'o' , '8': 'x' , '9': ' ' ,
             '4': ' ' , '5': 'x' , '6': ' ' ,
             '1': ' ' , '2': 'o' , '3': ' ' };
+
+ function printBoard() {
+    console.log(board['7'] + '|' + board['8'] + '|' + board['9'])
+    console.log('-+-+-')
+    console.log(board['4'] + '|' + board['5'] + '|' + board['6'])
+    console.log('-+-+-')
+    console.log(board['1'] + '|' + board['2'] + '|' + board['3'])
+};           
+
 let availableBoard = [];
 let acceptableAnswers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 function computerTurn() {
-    if (board["1"] === 'x' || board["1"] === 'o') availableBoard.push("1");
-    if (board["2"] === 'x' || board["2"] === 'o') availableBoard.push("2");
-    if (board["3"] === 'x' || board["3"] === 'o') availableBoard.push("3");
-    if (board["4"] === 'x' || board["4"] === 'o') availableBoard.push("4");
-    if (board["5"] === 'x' || board["5"] === 'o') availableBoard.push("5");
-    if (board["6"] === 'x' || board["6"] === 'o') availableBoard.push("6");
-    if (board["7"] === 'x' || board["7"] === 'o') availableBoard.push("7");
-    if (board["8"] === 'x' || board["8"] === 'o') availableBoard.push("8");
-    if (board["9"] === 'x' || board["9"] === 'o') availableBoard.push("9");
-    console.log(availableBoard)
-};
+    for(let i = 0; i < acceptableAnswers.length; ++i) {
+       number = i.toString();
+       if(board[number] === ' ') availableBoard.push(number); 
+    }
+    let computerTurnFinished = availableBoard[Math.floor(availableBoard.length * Math.random())];
+    board[computerTurnFinished] = "o";
+    printBoard();
+ }
+ computerTurn();
--- tictactoe.js
+++ tictactoe.js
@@ -4,7 +4,6 @@
 let winner = undefined;
 let gameEnd = false;
 let count = 0;
-let turn = "x";
 //Set out the board
 
 
@@ -37,43 +36,45 @@ const rl = readline.createInterface({
 // while(gameEnd === false) {
 //     doTurn();
 // };
-
-
-let answer = '';
+doTurn();
+let acceptableAnswers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 function doTurn() {
-    rl.question(`It's your turn, ${turn}.  Choose where to go: 1 = bl, 2 = bm, 3 = br, 4 = ml, 5 = m, 6 = mr, 7 = tl, 8 = tm, 9 = tr`, (answer) => {
-        rl.close();
-
+    console.log(`It's your turn. Choose where to go: 1 = bl, 2 = bm, 3 = br, 4 = ml, 5 = m, 6 = mr, 7 = tl, 8 = tm, 9 = tr`);
+    rl.question(``, (aanswer) => {
+        
+        function isAcceptableInput() {
+            for (let i = 0; i < acceptableAnswers.length; ++i) {
+                let inputNumber = acceptableAnswers[i];
+                if (inputNumber !== aanswer){
+                    if (inputNumber === 1) return board["1"] = "x";
+                    else if (inputNumber === 2) return board["2"] = "x";
+                    else if (inputNumber === 3) return board["3"] = "x";
+                    else if (inputNumber === 4) return board["4"] = "x";
+                    else if (inputNumber === 5) return board["5"] = "x";
+                    else if (inputNumber === 6) return board["6"] = "x";
+                    else if (inputNumber === 7) return board["7"] = "x";
+                    else if (inputNumber === 8) return board["8"] = "x";
+                    else if (inputNumber === 9) return board["9"] = "x";
+                } else {
+                     console.log("That was not an acceptable answer!")
+                };
+            };
+        };
+        
         // isAcceptableInput
-        isAcceptableInput(turn);
+        isAcceptableInput();
+        console.log(board);
         // checkIfWinner -- sets gameEnd = true and does return()
         playerWins();
         // doComputerTurn
-
+        computerTurn();
+        rl.close();
     });
 };
 
 
-function isAcceptableInput(userTurn) {
-    let acceptableAnswers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
-    for (let i = 0; i < acceptableAnswers.length; ++i) {
-        let inputNumber = acceptableAnswers[i];
-        if (inputNumber !==  answer){
-            console.log("That was not an acceptable answer!");
-        } else {
-            if (inputNumber === 1) board["1"] = userTurn;
-            else if (inputNumber === 2) board["2"] = userTurn;
-            else if (inputNumber === 3) board["3"] = userTurn;
-            else if (inputNumber === 4) board["4"] = userTurn;
-            else if (inputNumber === 5) board["5"] = userTurn;
-            else if (inputNumber === 6) board["6"] = userTurn;
-            else if (inputNumber === 7) board["7"] = userTurn;
-            else if (inputNumber === 8) board["8"] = userTurn;
-            else if (inputNumber === 9) board["9"] = userTurn;
-        };
-    };
-};
     // Determining a winner
+    let turn = "x";
 function playerWins() { // player_character is either 'x' or 'o'
     if(count < 5) return(false);
 
@@ -92,13 +93,20 @@ function playerWins() { // player_character is either 'x' or 'o'
     return(false);
  }
 
-
+ let availableBoard = [];
+ function computerTurn() {
+     for(let i = 0; i < acceptableAnswers.length; ++i) {
+        number = i.toString();
+        if(board[number] === ' ') availableBoard.push(number); 
+     }
+     let computerTurnFinished = availableBoard[Math.floor(availableBoard.length * Math.random())];
+     board[computerTurnFinished] = "o";
+  }
+  computerTurn();
 
 if (count >= 5) {
     
 };
-//Switch turns
-if (turn === "x") turn = "o";
 
 //Save the progress & restart
 if (gameEnd !== true) {
